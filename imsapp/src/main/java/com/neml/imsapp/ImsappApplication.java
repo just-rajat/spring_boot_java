ca91ce6d9d214ac0936bf84bf0be580d23e0b5b2
Rajat Bodankar <rbodankar@gmail.com>
2021-05-28 16:27:34 +0530

    new app

---
+++ imsapp/src/main/java/com/neml/imsapp/ImsappApplication.java
@@ -0,0 +1,13 @@
+package com.neml.imsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsappApplication.class, args);
+	}
+
+}
